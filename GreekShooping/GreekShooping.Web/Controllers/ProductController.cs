05d858d97828480c096150951fb8ff34653095e3
werleyss <werley_desouzasilva@hotmail.com>
2022-03-16 23:18:51 -0300

    productIndex

---
+++ GreekShooping/GreekShooping.Web/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using GreekShooping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreekShooping.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            var products = await _productService.FindAllProducts();
+
+            return View(products);
+        }
+    }
+}
--- GreekShooping/GreekShooping.Web/Models/ProductModel.cs
+++ GreekShooping/GreekShooping.Web/Models/ProductModel.cs
@@ -6,6 +6,7 @@
         public string? Name { get; set; }
         public string? Description { get; set; }
         public string? CategoryName { get; set; }
+        public decimal Price { get; set; }
         public string? ImageURL { get; set; }
     }
 }
