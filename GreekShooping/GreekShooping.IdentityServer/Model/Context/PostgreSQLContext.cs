ec951033ca1c3cc236355371882423af7264afdf
werley.souza <werley.souza@maxdata.com.br>
2024-06-21 13:30:07 -0300

    IdentityServer

---
+++ GreekShooping/GreekShooping.IdentityServer/Model/Context/PostgreSQLContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GreekShooping.IdentityServer.Model.Context
+{
+    public class PostgreSQLContext : IdentityDbContext<ApplicationUser>
+    {
+        public PostgreSQLContext(DbContextOptions<PostgreSQLContext> options) : base(options) { }
+
+    }
+}
